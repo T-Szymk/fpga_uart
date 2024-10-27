2361b71a475e3d40a1f406f5e97f9fb930db1e64
T-Szymk <ziggys_42@hotmail.com>
2024-10-27 14:36:07 +0000

    add bit-level cfg to register

--- rtl/register.v
+++ rtl/register.v
@@ -20,88 +20,60 @@
 
 module register #(
   parameter integer         REG_WIDTH          = 32,
-  parameter integer         READ_CLEAR         =  0,
   parameter [REG_WIDTH-1:0] READ_WRITE_PATTERN = {REG_WIDTH{1'b0}},
   parameter [REG_WIDTH-1:0] READ_CLEAR_PATTERN = {REG_WIDTH{1'b0}}
 ) (
-  input  wire                 clk_i,
-  input  wire                 rst_i,
-  input  wire                 wr_en_periph_i,
-  input  wire                 wr_en_cpu_i,
-  input  wire                 rd_en_cpu_i,
-  input  wire [REG_WIDTH-1:0] data_periph_i,
-  input  wire [REG_WIDTH-1:0] data_cpu_i,
+  input  wire                 clk_i,          //! Clock signal
+  input  wire                 rst_i,          //! Synch active-high reset
+  input  wire                 wr_en_periph_i, //! Write enable from peripheral
+  input  wire                 wr_en_cpu_i,    //! Write enable from CPU/Master
+  input  wire                 rd_en_cpu_i,    //! Read enable from CPU/Master
+  input  wire [REG_WIDTH-1:0] data_periph_i,  //! Data from peripheral
+  input  wire [REG_WIDTH-1:0] data_cpu_i,     //! Data from CPU
 
-  output wire                 updated_o,
-  output wire [REG_WIDTH-1:0] data_o
+  output wire [REG_WIDTH-1:0] data_o          //! Output data from register
 );
 
-  reg [REG_WIDTH-1:0] data_r    = {REG_WIDTH{1'b0}};
-  reg                 updated_r = 1'b0;
+  reg [REG_WIDTH-1:0] data_r = {REG_WIDTH{1'b0}};
 
   assign data_o = data_r;
 
-  // clear register contents when read by CPU
+  genvar bit_idx;
+
   generate
-    // clear register contents when read is active and no write
-    if (READ_CLEAR) begin : gen_read_clear
+
+    for (bit_idx = 0; bit_idx < REG_WIDTH-1; bit_idx=bit_idx+1) begin : gen_bit_cfg
 
       always @(posedge clk_i) begin
 
         if (rst_i) begin
 
-          data_r    <= {REG_WIDTH{1'b0}};
-          updated_r <= 1'b0;
+          data_r[bit_idx] <= 1'b0;
 
         end else begin
-
-          updated_r <= 1'b0;
 
           // prioritise writes from CPU side
           if (wr_en_cpu_i) begin
 
-            data_r    <= data_cpu_i;
-            updated_r <= 1'b1;
+            // only write if bit is R/W
+            if (READ_CLEAR_PATTERN[bit_idx]) begin
+              data_r[bit_idx] <= data_cpu_i[bit_idx];
+            end
 
           end else if (wr_en_periph_i) begin
 
-            data_r    <= data_periph_i;
-            updated_r <= 1'b1;
+            if (READ_CLEAR_PATTERN[bit_idx]) begin
+              data_r[bit_idx] <= data_periph_i[bit_idx];
+            end
 
-          // if data is read, clear contents
+          // if data is read and read clear cfg is set for bit, clear contents
           end else if (rd_en_cpu_i) begin
 
-            data_r <= {REG_WIDTH{1'b0}};
+            if (READ_CLEAR_PATTERN[bit_idx]) begin
+              data_r[bit_idx] <= 1'b0;
+            end
 
           end
-
-        end
-      end
-
-    end else begin : gen_no_read_clear
-
-      always @(posedge clk_i) begin
-
-        if (rst_i) begin
-
-          data_r    <= {REG_WIDTH{1'b0}};
-          updated_r <= 1'b0;
-
-        end else begin
-
-          updated_r <= 1'b0;
-
-          // prioritise writes from CPU side
-          if (wr_en_cpu_i) begin
-
-            data_r    <= data_cpu_i;
-            updated_r <= 1'b1;
-
-          end else if (wr_en_periph_i) begin
-            data_r    <= data_periph_i;
-            updated_r <= 1'b1;
-          end
-
         end
       end
     end
